beb3ec3983aa0f4eb60857580af75fccb8b5e326
Mavis <mavis@local>
2026-07-26 19:27:24 +0800

    ui: crarecreatedialog 1:1 port + 3 tests

---
+++ modern/src/ui/crarecreatedialog.hpp
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include "mxh/ui/cDialog.hpp"
+#include <cstdint>
+#include <functional>
+#include <vector>
+namespace mxh::ui {struct CreateMaterial{std::uint32_t id{};std::uint16_t count{};};class cRareCreateDialog final:public cDialog{public:using CreateCallback=std::function<bool(std::uint32_t)>;void SetRecipe(std::uint32_t result,std::vector<CreateMaterial> materials);bool SetMaterial(std::size_t slot,CreateMaterial material);bool CanCreate()const noexcept;bool Create();void SetCreateCallback(CreateCallback cb){m_callback=std::move(cb);}std::uint32_t ResultId()const noexcept{return m_result;}const std::vector<CreateMaterial>& Materials()const noexcept{return m_materials;}private:std::uint32_t m_result{};std::vector<CreateMaterial>m_materials;std::vector<bool>m_filled;CreateCallback m_callback;};}
---
+++ modern/tests/unit/ui/crarecreatedialog_test.cpp
@@ -0,0 +1,6 @@
+﻿#include "crarecreatedialog.hpp"
+#include <gtest/gtest.h>
+using namespace mxh::ui;
+TEST(RareCreateDialog, ValidatesMaterialsAndCreates){cRareCreateDialog d;d.SetRecipe(99,{{1,2},{2,1}});EXPECT_TRUE(d.SetMaterial(0,{1,2}));EXPECT_TRUE(d.SetMaterial(1,{2,1}));std::uint32_t result=0;d.SetCreateCallback([&](auto id){result=id;return true;});EXPECT_TRUE(d.CanCreate());EXPECT_TRUE(d.Create());EXPECT_EQ(result,99u);}
+TEST(RareCreateDialog, RejectsInsufficientMaterials){cRareCreateDialog d;d.SetRecipe(99,{{1,2}});EXPECT_FALSE(d.SetMaterial(0,{1,1}));EXPECT_FALSE(d.CanCreate());EXPECT_FALSE(d.Create());}
+TEST(RareCreateDialog, CallbackFailureDoesNotCorruptRecipe){cRareCreateDialog d;d.SetRecipe(99,{{1,1}});d.SetMaterial(0,{1,1});d.SetCreateCallback([](auto){return false;});EXPECT_FALSE(d.Create());EXPECT_TRUE(d.CanCreate());}
