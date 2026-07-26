f52f90225b7ddb0416fb658a7814b209a0fe6609
Mavis <mavis@local>
2026-07-26 19:27:20 +0800

    ui: cchatoptiondialog 1:1 port + 2 tests

---
+++ modern/src/ui/cchatoptiondialog.hpp
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include "mxh/ui/cDialog.hpp"
+#include <cstdint>
+namespace mxh::ui {class cChatOptionDialog final:public cDialog{public:void SetChatEnabled(bool v)noexcept{m_chat=v;}void SetWhisperEnabled(bool v)noexcept{m_whisper=v;}void SetFilterMask(std::uint32_t v)noexcept{m_filter=v;}bool ChatEnabled()const noexcept{return m_chat;}bool WhisperEnabled()const noexcept{return m_whisper;}std::uint32_t FilterMask()const noexcept{return m_filter;}void Reset()noexcept{m_chat=true;m_whisper=true;m_filter=0; }private:bool m_chat{true},m_whisper{true};std::uint32_t m_filter{};};}
---
+++ modern/tests/unit/ui/cchatoptiondialog_test.cpp
@@ -0,0 +1,5 @@
+﻿#include "cchatoptiondialog.hpp"
+#include <gtest/gtest.h>
+using namespace mxh::ui;
+TEST(ChatOptionDialog, StoresChatOptions){cChatOptionDialog d;d.SetChatEnabled(false);d.SetWhisperEnabled(false);d.SetFilterMask(7);EXPECT_FALSE(d.ChatEnabled());EXPECT_FALSE(d.WhisperEnabled());EXPECT_EQ(d.FilterMask(),7u);}
+TEST(ChatOptionDialog, ResetRestoresDefaults){cChatOptionDialog d;d.SetChatEnabled(false);d.SetFilterMask(99);d.Reset();EXPECT_TRUE(d.ChatEnabled());EXPECT_TRUE(d.WhisperEnabled());EXPECT_EQ(d.FilterMask(),0u);}
