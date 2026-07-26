8a3b24f4a3cd9b5df3b08c6170acad1a254fa02b
Mavis <mavis@local>
2026-07-26 19:29:12 +0800

    client: client_e2e_flow_test (6-state lifecycle E2E) + 1 tests

---
+++ modern/tests/unit/client/client_e2e_flow_test.cpp
@@ -0,0 +1,22 @@
+﻿#include "CEngine.hpp"
+#include "CMainGame.hpp"
+#include "CGameState.hpp"
+#include <gtest/gtest.h>
+using namespace mxh::client;
+namespace {
+class ProbeState final : public CGameState {
+public:
+ explicit ProbeState(int* enters, int* leaves): enters_(enters), leaves_(leaves) {}
+ void Init(void*) override { ++*enters_; setInitialized(true); }
+ void Release() override { ++*leaves_; setInitialized(false); }
+private: int* enters_; int* leaves_;
+};
+}
+TEST(ClientE2E, SixStateLifecyclePath) {
+ CMainGame game; game.Init(nullptr); int enters=0, leaves=0;
+ auto add=[&](GameStateId id){ game.RegisterState(id, std::make_unique<ProbeState>(&enters,&leaves)); };
+ add(GameStateId::Title); add(GameStateId::Connect); add(GameStateId::CharSelect); add(GameStateId::GameIn); add(GameStateId::MurimNet);
+ for (auto id: {GameStateId::Title,GameStateId::Connect,GameStateId::CharSelect,GameStateId::GameIn,GameStateId::MurimNet,GameStateId::Title}) { game.SetGameState(id); game.Process(); ASSERT_EQ(game.GetCurStateNum(),id); ASSERT_NE(game.GetCurGameState(),nullptr); }
+ EXPECT_EQ(enters,6); EXPECT_EQ(leaves,5); game.Release();
+}
+
