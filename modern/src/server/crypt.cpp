26be5964ff0e3d745a20da52e69f88d9e7b44f0b
Mavis <mavis@local>
2026-08-03 06:14:14 +0800

    bug: Crypt::encrypt_crc/decrypt_crc now return stream CRC char (was always 0)

--- modern/src/server/crypt.cpp
+++ modern/src/server/crypt.cpp
@@ -4,7 +4,13 @@ void Crypt::create(){mxh::crypto::HselInit i;i.iEncryptType=mxh::crypto::HSEL_EN
 void Crypt::init(const mxh::crypto::HselInit& en,const mxh::crypto::HselInit& de){en_init_=de;de_init_=en;en_stream_.initial(de);de_stream_.initial(en);initialized_=true;}
 bool Crypt::encrypt(char*d,int n){if(!initialized_)return true;return en_stream_.encrypt(d,n);}
 bool Crypt::decrypt(char*d,int n){if(!initialized_)return true;return de_stream_.decrypt(d,n);}
-char Crypt::encrypt_crc()const{return initialized_?0:0;}
-char Crypt::decrypt_crc()const{return initialized_?0:0;}
+char Crypt::encrypt_crc()const{
+  if(!initialized_)return 0;
+  return static_cast<char>(en_stream_.get_crc_char());
+}
+char Crypt::decrypt_crc()const{
+  if(!initialized_)return 0;
+  return static_cast<char>(de_stream_.get_crc_char());
+}
 }
 [[maybe_unused]] constexpr int crypt_translation_unit_anchor=0;
